6941fdac201ac26c2d56938ae7e7b260fa6bd1b6
Deepa Birari <112015631+deepa5053@users.noreply.github.com>
2023-03-07 14:50:58 +0100

    components created

---
+++ src/components/jagtap/Jagtap.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Jagtap = () => {
+  return (
+    <div>Jagtap</div>
+  )
+}
+
+export default Jagtap
---
+++ src/components/shimpi/Shimpi.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Shimpi = () => {
+  return (
+    <div>Shimpi</div>
+  )
+}
+
+export default Shimpi
